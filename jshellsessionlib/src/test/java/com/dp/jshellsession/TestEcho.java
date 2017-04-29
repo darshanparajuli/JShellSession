e5ab7901d28b9faf606c713536e038bf1adcf548
Darshan Parajuli <parajulidarshan@gmail.com>
2017-04-28 23:31:00 -0700

    removed quickrun test, not needed

--- jshellsessionlib/src/test/java/com/dp/jshellsession/TestEcho.java
+++ jshellsessionlib/src/test/java/com/dp/jshellsession/TestEcho.java
@@ -4,9 +4,8 @@
 
 package com.dp.jshellsession;
 
-import jshellsession.CommandOutput;
+import jshellsession.CommandResult;
 import jshellsession.Config;
-import jshellsession.JShell;
 import jshellsession.JShellSession;
 import org.junit.Assert;
 import org.junit.Test;
@@ -17,18 +16,7 @@ public class TestEcho {
 
     @Test
     public void testEchoHelloWorld() throws IOException {
-        Assert.assertTrue(JShellSession.init(Config.defaultConfig()));
-        final JShell jShellSession = JShellSession.getInstance();
-        final CommandOutput output = jShellSession.run("echo hello world");
-        Assert.assertTrue(output.exitSuccess());
-        Assert.assertTrue(output.stdOut().length == 1);
-        Assert.assertTrue(output.stdOut()[0].equals("hello world"));
-        JShellSession.destroy();
-    }
-
-    @Test
-    public void testQuickRun() throws IOException {
-        final CommandOutput output = JShellSession.quickRun(Config.defaultConfig(), "echo hello world");
+        final CommandResult output = JShellSession.quickRun(Config.defaultConfig(), "echo hello world");
         Assert.assertTrue(output.exitSuccess());
         Assert.assertTrue(output.stdOut().length == 1);
         Assert.assertTrue(output.stdOut()[0].equals("hello world"));
