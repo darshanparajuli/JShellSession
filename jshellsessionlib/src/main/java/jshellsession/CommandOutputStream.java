6e6c387b9218116a42a28b79a4c6b27431ad3068
Darshan Parajuli <parajulidarshan@gmail.com>
2017-04-28 23:28:45 -0700

    run CommandOutputStream in it's own thread; calling stdOutStream doesn't block

--- jshellsessionlib/src/main/java/jshellsession/CommandOutputStream.java
+++ jshellsessionlib/src/main/java/jshellsession/CommandOutputStream.java
@@ -4,26 +4,50 @@
 
 package jshellsession;
 
+import java.io.Closeable;
 import java.io.IOException;
 
-public class CommandOutputStream {
+public class CommandOutputStream implements Closeable {
 
-    private JShell mShell;
+    private JShellSession mShell;
+    private Thread mThread;
 
     public CommandOutputStream(Config config) throws IOException {
-        mShell = new JShell(config);
+        mShell = new JShellSession(config);
+        mThread = null;
     }
 
     public void stdOutStream(String cmd, OnCommandOutputListener listener) throws IOException {
         stdOutStream(cmd, 0, listener);
     }
 
-    public void stdOutStream(String cmd, long timeout, OnCommandOutputListener listener) throws IOException {
+    public void stdOutStream(final String cmd, final long timeout, OnCommandOutputListener listener) throws IOException {
+        if (mThread != null) {
+            throw new IllegalStateException("Object cannot be reused");
+        }
+
         mShell.setOnCommandOutputListener(listener);
-        mShell.run(cmd, timeout);
+        mThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mShell.run(cmd, timeout);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        mThread.start();
     }
 
+    @Override
     public void close() {
         mShell.close();
+        if (mThread != null) {
+            try {
+                mThread.join();
+            } catch (InterruptedException ignored) {
+            }
+        }
     }
 }
