adb5173ae0d796cd2d7779256f2ba77958203e5e
Darshan Parajuli <parajulidarshan@gmail.com>
2017-06-10 13:47:10 -0700

    added new example

---
+++ example/src/main/java/com/dp/examples/ProcInfo.java
@@ -0,0 +1,36 @@
+package com.dp.examples;
+
+import jshellsession.Config;
+import jshellsession.JShellSession;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProcInfo {
+
+    public static void main(String[] args) {
+        final Map<Integer, String> map = new HashMap<>();
+        try (final JShellSession session = new JShellSession(Config.defaultConfig())) {
+            for (String s : session.run("ls -1 /proc").stdOut()) {
+                try {
+                    final int pid = Integer.parseInt(s);
+                    final String cmdlinePath = String.format("/proc/%s/cmdline", s);
+                    final String[] stdOut = session.run("cat " + cmdlinePath).stdOut();
+                    if (stdOut.length > 0) {
+                        final String cmdline = stdOut[0].replace('\0', ' ').trim();
+                        map.put(pid, cmdline);
+                    }
+                } catch (NumberFormatException ignored) {
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (int pid : map.keySet()) {
+            System.out.printf("%6d => %s\n", pid, map.get(pid));
+        }
+    }
+
+}
